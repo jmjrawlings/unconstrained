1ab379c830d548ba1c524e6b93b0bec69ce0c75b
Justin Rawlings <hrkn@pm.me>
2022-04-21 09:06:36 +0000

    xd

--- tests/test_minizinc.py
+++ tests/test_minizinc.py
@@ -2,6 +2,39 @@ from unconstrained import *
 from pytest import mark, fixture
 
 
+SOLVERS = [
+    ORTOOLS,
+    #CHUFFED,
+    #GECODE,
+    #COINBC,
+]
+
+THREADS = [
+    # 0,
+    # 2,
+    # 4,
+    8
+]
+
+
+@fixture(params=SOLVERS)
+def minizinc_solver(request) -> Solver:
+    return get_solver(request.param)
+
+
+@fixture(params=THREADS)
+def minizinc_threads(request) -> int:
+    return request.param
+
+
+@fixture
+def minizinc_options(minizinc_solver, minizinc_threads):
+    return MiniZincOptions(
+        solver_id = minizinc_solver.id,
+        threads = minizinc_threads
+    )
+
+
 async def test_solve_simple_satisfy_model(minizinc_options):
     result = await best_minizinc_solution(
         """
